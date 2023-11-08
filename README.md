e1e8c88a84f95c40d684f84e304694ffe9559df0
stefan <stefan.zimmermann@x-plizit.de>
2023-11-08 16:50:53 +0100

    fixed time frames

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 Generate simple TOTP-Codes for multi-factor purpoises and validate them quickly.
 
+# Example
+```
+use StefanZ\LaravelTOTP\TOTP;
+
+$token = (new TOTP('sha256', 0, 600))->GenerateToken('SUPER-SECRET', NULL, 8);
+
+```
+
+```
+
+if (Validator::validate('SUPER-SECRET', 'XXXXXXXX', ['sha256', 0, 600])) {
+    // Token is valid
+}
+
+
+```
 
 
 
--- src/Validator.php
+++ src/Validator.php
@@ -4,11 +4,11 @@ namespace StefanZ\LaravelTOTP;
 
 class Validator
 {
-    public static function validate(string $key = "", string $token = "")
+    public static function validate(string $key = "", string $token = "", $config = ['sha256', 0, 600])
     {
         if (strlen($key) < 62 || strlen($token) > 6) return false;
         if (strlen($token) < 6) $token = str_pad($token, "0", STR_PAD_LEFT);
-        $current = (new TOTP())->GenerateToken($key);
+        $current = (new TOTP($config[0], $config[1], $config[2]))->GenerateToken($key);
         return ($current === $token);
     }
 }
